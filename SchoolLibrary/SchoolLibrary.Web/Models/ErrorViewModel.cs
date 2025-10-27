cb4aacdebd4b07eb4357cd5d3054906be22947ec
PhanVanDinh <vonhanh271@gmail.com>
2025-10-27 23:43:40 +0700

    update project

---
+++ SchoolLibrary/SchoolLibrary.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchoolLibrary.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
